98aa563c3acb3fd6c6933533624c0c63c082ec13
salohiddinlife <z1565400.com>
2024-02-26 16:47:33 +0500

    first commited

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <RouterView/>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from "@/components/Header/Header.vue";
+import Footer from "@/components/Footer/Footer.vue";
+</script>
+
+<style lang="scss" scoped></style>
